8958118e9285ef171a8e359bbc4bf55274f4967f
abirhasan33 <abir03512@gmail.com>
2022-05-26 21:38:42 +0600

    Pemant crect outilne

--- index.js
+++ index.js
@@ -1,11 +1,207 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_SERCRT_KEY);
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n5jjz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const servicesCollecttion = client.db('assignment-12').collection('service');
+        const bookingCollection = client.db('assignment-12').collection('bookings');
+        const userCollection = client.db('assignment-12').collection('users');
+        const paymentCollection = client.db('assignment-12').collection('payments');
+
+
+        function verifyJWT(req, res, next) {
+            const authHeader = req.headers.authorization;
+            if (!authHeader) {
+              return res.status(401).send({ message: 'UnAuthorized access' });
+            }
+            const token = authHeader.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+              if (err) {
+                return res.status(403).send({ message: 'Forbidden access' })
+              }
+              req.decoded = decoded;
+              next();
+            });
+          }
+
+          app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+          });
+
+           // DELETE
+         app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+          app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+              const filter = { email: email };
+              const updateDoc = {
+                $set: { role: 'admin' },
+              };
+              const result = await userCollection.updateOne(filter, updateDoc);
+              res.send(result);
+            }
+            else{
+              res.status(403).send({message: 'forbidden'});
+            }
+      
+          });
+
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+              $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
+          });
+
+
+          app.get('/admin/:email', async(req, res) =>{
+            const email = req.params.email;
+            const user = await userCollection.findOne({email: email});
+            const isAdmin = user.role === 'admin';
+            res.send({admin: isAdmin})
+          })
+
+
+
+          app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { ordersId: booking.ordersId, user: booking.user }
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+              return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingCollection.insertOne(booking);
+            console.log('sending email');
+            sendAppointmentEmail(booking);
+            return res.send({ success: true, result });
+          });
+
+
+          app.get('/booking/:id', verifyJWT, async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const booking = await bookingCollection.findOne(query);
+            res.send(booking);
+          });
+
+          app.patch('/booking/:id', verifyJWT, async(req, res) =>{
+            const id  = req.params.id;
+            const payment = req.body;
+            const filter = {_id: ObjectId(id)};
+            const updatedDoc = {
+              $set: {
+                paid: true,
+                transactionId: payment.transactionId
+              }
+            }
+      
+            const result = await paymentCollection.insertOne(payment);
+            const updatedBooking = await bookingCollection.updateOne(filter, updatedDoc);
+            res.send(updatedBooking);
+          });
+
+
+
+          app.get('/available', async (req, res) => {
+            const bookings = req.query.bookings;
+      
+            const services = await servicesCollecttion.find().toArray();
+      
+            const query = { bookings: bookings };
+            const booking = await bookingCollection.find(query).toArray();
+            res.send(booking);
+          });
+
+
+        app.get('/service', async(req, res)=> {
+            const query = {};
+            const cursor = servicesCollecttion.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/service/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await servicesCollecttion.findOne(query);
+            res.send(service);
+        });
+
+        // PUT 
+        app.put('/service/:id', async(req, res)=> {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const quantit = req.body.quantity;
+            const option = {upserd: true}
+            const quantityMinus = {
+                $set: {
+                    quantity: quantit
+                }
+            }
+            
+            const quantityUpedate = await servicesCollecttion.updateOne(query, quantityMinus, option);
+            res.send({quantityUpedate})
+        });
+
+
+        app.post('/create-payment-intent', verifyJWT, async(req, res) =>{
+            const service = req.body;
+            const price = service.price;
+            const amount = price*100;
+            const paymentIntent = await stripe.paymentIntents.create({
+              amount : amount,
+              currency: 'usd',
+              payment_method_types:['card']
+            });
+            res.send({clientSecret: paymentIntent.client_secret})
+          });
+
+
+
+          app.post('/service', async (req, res) => {
+            const newService = req.body;
+            const result = await servicesCollecttion.insertOne(newService);
+            res.send(result);
+        });
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World assignment-12-server')
