4047080c73e04a1e884b3e5c90b5a30a3a517879
abirhasan33 <abir03512@gmail.com>
2022-06-02 17:37:03 +0600

    new commit

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const app = express()
 const port = process.env.PORT || 5000;
 const stripe = require('stripe')(process.env.STRIPE_SERCRT_KEY);
 
-// app.use(cors());
+app.use(cors());
 // const corsConfig = {
 //   origin: 'https://assignment-12-7020e.web.app',
 //   credentials: true,
 // }
-app.use(cors({origin: 'https://assignment-12-7020e.web.app'}))
+// app.use(cors({origin: 'https://assignment-12-7020e.web.app'}))
 
 // app.use(cors(corsConfig))
 app.use(express.json());
